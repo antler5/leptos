75e12962f811bec95529777aaf4f64ab01bf574c
antlers <antlers@illucid.net>
2026-08-13 23:29:12 -0700

    islands-router: extend test to attach & click button
    I don't think navigation is supposed to attach (and detach) 1 elm at-a-time?
    
    this started with:
    b20902aaa11b8eaf37a2ebf22e9b75ea705c426b (https://github.com/leptos-rs/leptos/issues/4378)
    
    ```
      ✓  1 [chromium] › tests/navigation.spec.ts:16:5 › navigates from Home to About (once for each elm on About) and clicks the Button (2.2s)
    ```

--- examples/islands_router/e2e/tests/navigation.spec.ts
+++ examples/islands_router/e2e/tests/navigation.spec.ts
@@ -13,18 +13,29 @@ function collectErrors(page: Page): string[] {
   return errors;
 }
 
-test("navigates from Home to About", async ({ page }) => {
+test("navigates from Home to About (once for each elm on About) and clicks the Button", async ({ page }) => {
   const errors = collectErrors(page);
 
   await page.goto("/");
   await expect(page).toHaveTitle("Home | My Contacts");
   await expect(page.locator("input[type=search]")).toBeVisible();
 
-  await page.click('nav a[href="/about"]');
+  await page.click('nav a[href="/about"]'); // 1
   await expect(page).toHaveURL(/\/about$/);
   await expect(page.getByRole("heading", { name: "About" })).toBeVisible();
   await expect(page).toHaveTitle("About | My Contacts");
   await expect(page.locator("input[type=search]")).not.toBeAttached();
+
+  await expect(page.locator("button[class=counter]")).not.toBeAttached();
+  await page.click('nav a[href="/about"]'); // 2
+  await expect(page.locator("button[class=counter]")).not.toBeAttached();
+  await page.click('nav a[href="/about"]'); // 3
+  await expect(page.locator("button[class=counter]")).not.toBeAttached();
+  await page.click('nav a[href="/about"]'); // 4
+  await expect(page.locator("button[class=counter]")).toBeAttached();
+  await expect(page.locator("button[class=counter]")).toHaveText("0");
+  await page.click('button[class=counter]'); // 5
+  await expect(page.locator("button[class=counter]")).toHaveText("1");
 
   expect(errors).toEqual([]);
 });
